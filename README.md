bb8d85240f4e7afe1da50d5d8f2f9b1cca623f30
ecozbel <73610579+ecozbel@users.noreply.github.com>
2022-02-22 11:18:05 -0600

    ver 1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EmailAutomation
+ This script is for the internal use of Istanbul 
+ Exporters' Association
+ 
+Reads given excel file and extracts the full name and 
+email address of recipient. Composes an email
+that includes the full name of the recipient in the 
+text body as well as an html brochure.
  
--- v2.py
+++ v2.py
@@ -16,7 +16,7 @@ all_messages = email_list['Message']
 
 
 msg = MIMEMultipart('mixed')
-report_file = open('IIFF Haziran.html')
+report_file = open(filename)
 html = report_file.read()
 
 text = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>"
